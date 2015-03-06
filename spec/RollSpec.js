d8efd98af75db29b0b5fba75ffb8d32d4435e0d2
Erik Griffin <griffin.erik@gmail.com>
2015-03-06 12:14:14 +0000

    Added roll

---
+++ spec/RollSpec.js
@@ -0,0 +1,16 @@
+describe("Roll", function() {
+
+  var roll;
+
+  beforeEach(function () {
+    roll = new Roll(6);
+  });
+
+  it("Is initialized with its score, in this case '6'", function() {
+    expect(roll.score).toEqual(6);
+  });
+
+
+
+
+});
